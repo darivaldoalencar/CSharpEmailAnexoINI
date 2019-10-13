83a34423ae57535bddce2f00f46cc4e691c8ba88
darivaldo <darivaldo@hotmail.com>
2019-10-13 03:42:34 -0300

    #master - envio de fontes

---
+++ Email/IniFile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Email
+{
+    public class IniFile
+    {
+        public string Path { get; set; }
+
+        public IniFile(string iniPath)
+        {
+            Path = iniPath;
+        }
+
+        public void WriteValue(string section, string key, string value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value, Path);
+        }
+
+        public string ReadValue(string section, string key, string Default)
+        {
+            var buffer = new StringBuilder(255);
+            NativeMethods.GetPrivateProfileString(section, key, Default, buffer, 255, Path);
+
+            return buffer.ToString();
+        }
+
+        public void WriteValue(string section, string key, int value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value.ToString(CultureInfo.InvariantCulture), Path);
+        }
+
+        public int ReadValue(string section, string key, int Default)
+        {
+            var buffer = new StringBuilder(255);
+            NativeMethods.GetPrivateProfileString(section, key, Default.ToString(CultureInfo.InvariantCulture), buffer, 255, Path);
+
+            return int.Parse(buffer.ToString());
+        }
+
+        public void WriteValue(string section, string key, UInt16 value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value.ToString(CultureInfo.InvariantCulture), Path);
+        }
+        public UInt16 ReadValue(string section, string key, UInt16 Default)
+        {
+            var buffer = new StringBuilder(255);
+            NativeMethods.GetPrivateProfileString(section, key, Default.ToString(CultureInfo.InvariantCulture), buffer, 255, Path);
+
+            return UInt16.Parse(buffer.ToString());
+        }
+        public void WriteValue(string section, string key, UInt32 value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value.ToString(CultureInfo.InvariantCulture), Path);
+        }
+        public UInt32 ReadValue(string section, string key, UInt32 Default)
+        {
+            var buffer = new StringBuilder(255);
+            NativeMethods.GetPrivateProfileString(section, key, Default.ToString(CultureInfo.InvariantCulture), buffer, 255, Path);
+
+            return UInt32.Parse(buffer.ToString());
+        }
+
+        public bool ReadValue(string section, string key, bool Default)
+        {
+            var buffer = new StringBuilder(255);
+            NativeMethods.GetPrivateProfileString(section, key, Default.ToString(CultureInfo.InvariantCulture), buffer, 255, Path);
+
+            return (buffer.ToString() != "False");
+        }
+        public void WriteValue(string section, string key, bool value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value.ToString(CultureInfo.InvariantCulture), Path);
+        }
+
+        static class NativeMethods
+        {
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            public static extern uint WritePrivateProfileString(string section,
+            string key, string val, string filePath);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            public static extern int GetPrivateProfileString(string section,
+            string key, string def, StringBuilder retVal, int size,
+            string filePath);
+        }
+
+        public string getTAG(string tag)
+        {
+            var leitura = new IniFile(this.Path).ReadValue("CONFIGURACOES", tag, string.Empty); ;                 
+            return leitura;
+        }
+
+    }
+}
---
+++ Email/Program.cs
@@ -0,0 +1,159 @@
+﻿using Limilabs.Client.IMAP;
+using Limilabs.Client.POP3;
+using Limilabs.Mail;
+using Limilabs.Mail.MIME;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Email
+{
+    class Program
+    {
+        //Instalar as DLLs: PM> Install-Package Mail.dll
+        //https://www.limilabs.com/mail        
+
+        #region propriedades
+        private IniFile iniFile
+        {
+            get
+            {                
+                return new IniFile(pathINI);
+            }
+        }
+
+        private static string pathINI
+        {
+            get
+            {                
+                return "C:\\ProjetosWeb\\CSharpEmailAnexoINI\\Email\\configuracoes\\CONFIGURACOES.INI";
+            }
+        }
+
+        private string MeuEmaail
+        {
+            get
+            {
+                return iniFile.getTAG("EMAIL");
+            }
+        }
+
+        private string MinhaSenha
+        {
+            get
+            {
+                return iniFile.getTAG("SENHA");
+            }
+        }
+        private string PathAnexo
+        {
+            get
+            {
+                string PathAnexo = iniFile.getTAG("PATHANEXO");
+                if (!PathAnexo[PathAnexo.Length - 1].ToString().Equals("\\"))
+                {
+                    PathAnexo +=  "\\";
+                }
+                CriarPastasAmbiente(PathAnexo);
+                return PathAnexo;
+            }
+        }
+        private string hostPop3
+        {
+            get
+            {
+                return iniFile.getTAG("HOSTPOP3");
+            }
+        }
+
+        private string hostIMAP
+        {
+            get
+            {
+                return iniFile.getTAG("HOSTIMAP");
+            }
+        }
+        #endregion
+
+        #region métodos
+        private void BaixarPop3()
+        {
+            using (Pop3 pop3 = new Pop3())
+            {
+                //pop3.Connect("host sem SSL");  
+                pop3.ConnectSSL(hostPop3);
+                pop3.UseBestLogin(MeuEmaail, MinhaSenha);
+
+                foreach (string uid in pop3.GetAll())
+                {
+                    IMail email = new MailBuilder()
+                        .CreateFromEml(pop3.GetMessageByUID(uid));
+
+                    Console.WriteLine(email.Subject);
+
+                    // salva anexo no disco
+                    foreach (MimeData mime in email.Attachments)
+                    {
+                        mime.Save(string.Concat(PathAnexo, mime.SafeFileName));
+                    }
+                }
+                pop3.Close();
+            }
+
+        }
+
+        public void BaixarImap()
+        {
+            using (Imap imap = new Imap())
+            {
+                //imap.Connect("host sem SSL);   
+                imap.ConnectSSL(hostIMAP);
+                imap.UseBestLogin(MeuEmaail, MinhaSenha);
+
+                imap.SelectInbox();
+                List<long> uids = imap.Search(Flag.All);
+
+                foreach (long uid in uids)
+                {
+                    IMail email = new MailBuilder()
+                        .CreateFromEml(imap.GetMessageByUID(uid));
+
+                    Console.WriteLine(email.Subject);
+
+                    // salva anexo no disco
+                    foreach (MimeData mime in email.Attachments)
+                    {
+                        mime.Save(string.Concat(PathAnexo, mime.SafeFileName));
+                    }
+                }
+            }
+        }        
+
+        private bool CriarPastasAmbiente(string pasta)
+        {
+            try
+            {
+                if (!Directory.Exists(pasta))
+                {
+                    Directory.CreateDirectory(pasta);
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erro: " + ex.Message);
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {            
+            var p = new Program();
+            
+            //a versão pop3 funciona também
+            //p.BaixarPop3();
+
+            p.BaixarImap();            
+        }
+    }
+}
